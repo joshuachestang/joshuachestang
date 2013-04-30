065408b0b6165b0e86f94b242417cf9d158c73d5
Joshua Chestang <joshua.chestang@gmail.com>
2013-04-30 17:07:25 -0500

    first!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joshuachestang::Application.config.secret_token = '99bcd1eb55ed1725e74b65617c52f8e355908dc95c0afefc7a20cf0a27f31133dfd5e3e76ca336099fe6d40717255ce5367f471e5ef829ed4a2f675093af2d4a'
