bfc08bf2357b0b1e9c256d6e1bd52ac664175a13
Joshua Chestang <joshua.chestang@gmail.com>
2013-06-10 15:35:04 -0500

    setting up a cool layout

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Joshuachestang::Application.routes.draw do
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+    # match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
@@ -38,6 +38,7 @@ Joshuachestang::Application.routes.draw do
   #       get 'recent', :on => :collection
   #     end
   #   end
+  get("/home", :controller => "HomePages", :action => 'home', :as => 'home')
 
   # Sample resource route within a namespace:
   #   namespace :admin do
@@ -48,7 +49,7 @@ Joshuachestang::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'HomePages#home'
 
   # See how all your routes lay out with "rake routes"
 
